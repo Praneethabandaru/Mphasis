9580cc64ce5a900966a33a9958cf2cb7c26cfd96
Praneethabandaru <143315464+Praneethabandaru@users.noreply.github.com>
2025-05-23 17:52:10 +0530

    Add files via upload

---
+++ dotnetcore day-2/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace yourlib;
+
+public class Mylibcls
+{
+   public void Add(int x,int y)
+   {
+        Console.WriteLine($"The Sum is {x+y}");
+   } 
+    public void Multiply(int x,int y)
+    {
+        Console.WriteLine($"The Multiplication is {x*y}");
+    }
+}
---
+++ dotnetcore day-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using yourlib;
+using Newtonsoft.Json;
+Console.WriteLine("Hello, World!");
+// Mylibcls obj = new Mylibcls();
+// obj.Add(10,20);
+// obj.Multiply(10,20);
+
+var data = new 
+{
+    studentid=101,
+    studentname="Rahul",
+    studentaddress="USA"
+};
+Console.WriteLine($"{data.studentid} {data.studentname} {data.studentaddress}");
+string json = JsonConvert.SerializeObject(data,Formatting.Indented);
+System.Console.WriteLine(json);
